0580c8b891cf218e508e84df034e532e15c9fff4
kovalenkosavva27 <90443552+kovalenkosavva27@users.noreply.github.com>
2022-02-14 13:06:24 +0300

    Add files via upload

---
+++ scr/galary.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  
+  $(".slider").slick({
+    
+    infinite: true,
+       
+ slidesToShow: 1,
+        
+slidesToScroll: 1,
+
+ dots: true,
+    
+    responsive: [
+{
+
+breakpoint: 480,
+ 
+settings: {
+ slidesToShow: 2,
+ slidesToScroll: 2
+ }
+}]
+
+});
+
+});
